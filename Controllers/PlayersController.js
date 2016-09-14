acbc20613c349d987b4c8d2452085501eb06a214
Madushan D. Memmendarachchi <damiththa@gmail.com>
2016-09-14 15:22:01 -0400

    getting players

--- Controllers/PlayersController.js
+++ Controllers/PlayersController.js
@@ -1,10 +1,23 @@
 (function () {
     "use strict";
-    var PlayersController = function ($scope) {    
-             
+    var PlayersController = function ($scope, SecretsService, NFLService) {    
+        var Secret_return = SecretsService.getKeys();
+        Secret_return.then(function(data){
+            $scope.Secrets = data;
+            $scope.FireBase_secrets = $scope.Secrets.data.urls.FireBase; //FireBase
+            $scope.NFLPlayers_secrets = $scope.Secrets.data.urls.NFL_Players; //All NFL players, all positions
+
+            $scope.getPlayers = function(position){
+                var getNFLPlayers_ThisPosition = NFLService.getThisPosition($scope.NFLPlayers_secrets,position);
+                getNFLPlayers_ThisPosition.then(function(data){
+                    $scope.ThisPosition = data;
+                    console.log($scope.ThisPosition);
+                })         
+            };
+        });                  
     };
     
-    PlayersController.$inject = ['$scope'];
+    PlayersController.$inject = ['$scope', 'SecretsService'];
     
     angular.module('appFantasyF')
         .controller('PlayersController', PlayersController);    
