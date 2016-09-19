523e0a6cbd7dcbd63c9f6bee87e529d161df36f2
Madushan D. Memmendarachchi <damiththa@gmail.com>
2016-09-19 11:15:04 -0400

    getting and loading default position on load

--- Controllers/PlayersController.js
+++ Controllers/PlayersController.js
@@ -7,13 +7,19 @@
             $scope.FireBase_secrets = $scope.Secrets.data.urls.FireBase; //FireBase
             $scope.NFLPlayers_secrets = $scope.Secrets.data.urls.NFL_Players; //All NFL players, all positions
 
-            $scope.getPlayers = function(position){
+            $scope.getPlayers = function(position){ //Get NFL players accordingly
                 var getNFLPlayers_ThisPosition = NFLService.getThisPosition($scope.NFLPlayers_secrets,position);
                 getNFLPlayers_ThisPosition.then(function(data){
                     $scope.ThisPosition = data;
                     console.log($scope.ThisPosition);
                 })         
             };
+
+            //inti
+            function init(){
+                $scope.getPlayers('QB');    
+            };
+            init();
         });                  
     };
     
